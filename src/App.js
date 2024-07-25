cf5924fd8a1c5439cc3e914b2972d98ae64fb7c7
ChinmayRPatel <chinmayrpatel1309@gmail.com>
2024-07-25 10:50:10 +0530

    git commit message example

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Navigation from "./customer/components/Navigation/Navigation.jsx";
 import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/footer/Footer";
 import Product from "./customer/components/Product/Product.jsx";
-
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails"; 
 function App() {
   return (
     <div className="App relative">
       <Navigation />
-      <div className="mt-16"> {/* Adjust the margin to prevent content from hiding behind the navbar */}
+      <div className="mt-50"> {/* Adjust the margin to prevent content from hiding behind the navbar */}
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product className='lex flex-wrap justify-center'/> */}
+        <ProductDetails/>
+
       </div>
       <Footer />
     </div>
--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const ProductCard = () => {
+import './ProductCard.css'
+
+const ProductCard = ({product}) => {
   return (
-    <div>
-      
+    <div className='productCard w-[15rem] m-3 transition-all cursor-pinter'>
+      <div className='h-[20rem]'>
+      <img className='h-full w-full object-cover object-left-top' src={product.imageUrl} alt='' />
+      </div>
+      <div className="textPart bg-white p-1">
+        <p className='font-bold opacity-60'>{product.brand}</p>
+        <p>{product.title}</p>
+      </div>
+      <div className='flex item-center space-x-2 ml-6'>
+        <p className='font-semibold'>{product.discountedPrice}</p>
+        <p className='line-through opacity-50'>{product.price}</p>
+        <p className='text-green-600 font-semibold'>{product.discountPersent}</p>
+      </div>
     </div>
   )
 }
